65b55f4b0fc5d0c639fe43ed8c2fe0821bc9400e
szisz <szisz1982@gmail.com>
2016-01-21 13:58:54 +0100

    Project init Dev day 2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Gábor' });
+  res.render('index', { title: 'Gabor' });
 });
 
 module.exports = router;
